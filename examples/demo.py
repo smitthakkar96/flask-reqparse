0a98d17b7631e6e50efdf853797c4691084a42a5
smit thakkar <smitthakkar96@gmail.com>
2017-01-27 12:38:19 +0530

    spereated business logic from argument parsing

--- examples/demo.py
+++ examples/demo.py
@@ -5,19 +5,32 @@ from flask import Flask, jsonify
 sys.path.insert(0, '../')
 from flask_reqparse import RequestParser
 
-
+parser = RequestParser()
 
 app = Flask(__name__)
 
 @app.route('/api/json', methods=['POST'])
-def json_demo():
-    parser = RequestParser()
-    parser.add_argument('name', required=True)
-    parser.add_argument('email', required=True, help="email is required")
-    args = parser.parse_args()
-    print(args['name'])
-    print(args['email'])
-    return jsonify({"response" : "success"})
+@parser.validate_arguments([
+    {
+        'name' : "name",
+        'required' : True,
+        'source' : 'args'
+    }
+])
+def json_demo(args):
+    return jsonify({"response" : "hello " + args['name']})
+
+@app.route('/api/args_demo')
+@parser.validate_arguments([
+    {
+        'name' : "name",
+        'required' : True,
+        'type' : list
+    }
+])
+def args_demo(args):
+    return jsonify({"response" : args['name']})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
